8ce88ed08dfb67ac1b3417ee11eb01c5071bf2f5
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2026-08-06 20:57:02 +0800

    chore: install mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ntuhpc.org',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [mdx()]
 });
