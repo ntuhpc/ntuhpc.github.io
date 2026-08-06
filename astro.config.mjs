37681517130144be0680bc54151f2ddffedbc7d4
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2026-08-06 14:46:40 +0800

    chore: install astro, tailwindcss
    also setup theme colors

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ntuhpc.org',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
